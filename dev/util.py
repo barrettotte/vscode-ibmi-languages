e6c1cf0b541f77f23abd7c32b257a85a741facc9
barrettotte <barrettotte@gmail.com>
2026-07-29 01:35:51 -0400

    removes old/deprecated dev directory

--- dev/util.py
+++
@@ -1,8 +0,0 @@
-# take list of opcodes, reverse it, and build regex
-
-ops = []
-with open('sql/keywords.txt', 'r') as f:
-    for line in f:
-        ops.append(line.strip())
-with open('sql/regex.json', 'w') as f:
-    f.write('"' + ('|'.join(sorted(ops, reverse=True))) + '"')
